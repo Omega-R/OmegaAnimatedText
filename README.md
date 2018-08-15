22f678755b9f0f7d86473421c2a4e6c293f741db
Nikita Ivanov <scrinoman@gmail.com>
2018-08-15 12:47:02 +0300

    Created library

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# OmegaAnimatedText
-Animate bold and italic transformations in TextView
+
+# AnimatedText
+Android lib to animate bold and italic transformations in TextView.
+
+# Use
+Add dependencies to your gradle file.
+
+Project build.gradle:
+``` groovy
+allprojects {
+repositories {
+maven { url 'https://jitpack.io' }
+}
+}
+```
+
+App module build.gradle:
+``` groovy
+dependencies {
+implementation '[insert]'
+}
+```
+
+Start animation:
+``` Java
+mTextView.createStrokeAnimator(0.05f).start();
+mTextView.createItalicAnimator(0.4f)
+        .setDuration(1000)
+        .start();
+```
+
+# Note
+createStrokeAnimator(...) takes float percentage of textSize as first parameter.
+
+createItalicAnimator(...) takes float percentage of skew angle (from 0.0f for 0&deg; to 1.0f for 90&deg;) as first parameter.
+
+Both methods returns android.animation.Animator instance so you can make animations more complicated.
---
+++ app/src/main/java/com/omega/omegaanimatedtext/MainActivity.java
@@ -0,0 +1,47 @@
+package com.omega.omegaanimatedtext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.omega.animatedtext.AnimatedTextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private AnimatedTextView mTextView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.fwd_button).setOnClickListener(this);
+        findViewById(R.id.fwd_italic_button).setOnClickListener(this);
+        findViewById(R.id.back_button).setOnClickListener(this);
+        findViewById(R.id.back_italic_button).setOnClickListener(this);
+        mTextView = findViewById(R.id.strokeTextView);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case (R.id.fwd_button):
+                mTextView.createStrokeAnimator(0.05f).start();
+                break;
+            case (R.id.fwd_italic_button):
+                mTextView.createItalicAnimator(0.4f)
+                        .setDuration(1000)
+                        .start();
+                break;
+            case (R.id.back_button):
+                mTextView.createStrokeAnimator(0.0f).start();
+                break;
+            case (R.id.back_italic_button):
+                mTextView.createItalicAnimator(0.0f)
+                        .setDuration(1000)
+                        .start();
+                break;
+            default:
+                break;
+        }
+    }
+}
